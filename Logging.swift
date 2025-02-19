10b11a1dca59afb70c66840572c249e455774c49
Remy Skelton <remy.skelton@stu.ocsb.ca>
2025-02-19 04:37:39 +0000

    Done Logging swift

--- Logging.swift
+++ Logging.swift
@@ -7,7 +7,7 @@
 //
 //  This program will calculate the maximum number of logs that can fit into a truck based on certain lengths.
 
-// import foundation
+// Import Foundation library
 import Foundation
 
 // Constants
@@ -20,7 +20,7 @@ func Logging() {
     // Welcome message
     print("This program will calculate the maximum number of logs that can fit into a truck based on certain lengths.")
 
-    // Declare log length variable as double
+    // Declare log length variable as double and hold no value
     var logLengthDouble: Double?
 
     // Repeat until valid input
@@ -28,11 +28,18 @@ func Logging() {
         // Ask user for log length
         print("Please enter the length of the logs in meters (0.25, 0.5, or 1.0):")
 
-        // Read user input and convert to double with using guard
-        guard let logLengthAsString = readLine(), let logLengthDoubleLoop = Double(logLengthAsString) else {
+        // Read user input with guard
+        guard let logLengthAsString = readLine() else {
             print("Invalid input. Please enter a valid log length.")
             // exit guard and stay in loop
-            continue
+            return
+        }
+
+        // Safely convert user input to double
+        guard let logLengthDoubleLoop = Double(logLengthAsString) else {
+            print("Invalid input: \(logLengthAsString). Log length must be 0.25, 0.5, or 1.0.")
+            // exit guard and stay in loop
+            return
         }
 
         // Check if log length is valid
@@ -42,18 +49,18 @@ func Logging() {
             // Exit loop
             break
         } else {
-            print("Invalid input: \(logLengthDoubleLoop). Log length must be 0.25, 0.5, or 1.")
+            print("Invalid input: \(logLengthDoubleLoop). Log length must be 0.25, 0.5, or 1.0.")
         }
     } while true
 
-    // Calculate weight per log
-    let weightPerLog: Double = logLengthDouble * 20
+    // Calculate weight per log and force unwrap because it is an optional variable that does not contain nil
+    let weightPerLog: Double = logLengthDouble! * 20
 
-    // Calculate the number of logs
-    let numLogsInt: Int = Int(Double(MAX_WEIGHT) / weightPerLog)  // Convert result to Int
+    // Calculate the number of logs and make sure it is an integer
+    let numLogsInt: Int = Int(Double(MAX_WEIGHT) / weightPerLog)
 
     // Display the max number of logs
-    print("The maximum number of logs that can fit into a truck with \(logLengthDouble)m logs is \(numLogsInt) logs.")
+    print("The maximum number of logs that can fit into a truck with \(logLengthDouble!)m logs is \(numLogsInt) logs.")
 }
 
 // Call function
